f7e597320210b0e42131368f40e09ace0e0eaed5
Jeff Lee <leejeffc@gmail.com>
2013-12-09 15:04:33 -0800

    Remove redundant code (event chain will take care of this)

--- www/js/models/map.js
+++ www/js/models/map.js
@@ -318,7 +318,6 @@ define(['backbone'], function(Backbone){
             powerUpID: marker.id
           };
           if (marker.title === 'respawn') {
-            this.setPlayerAlive(data.playerName);
             this.get('socket').emit('playerRespawn', data);
           } else {
             this.get('socket').emit('addItemToPlayer', data);
