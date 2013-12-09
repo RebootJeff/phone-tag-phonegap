9bba64e420856b42331cec161a5303e464a613e4
Ryan Roxas <ryanaroxas@gmail.com>
2013-12-08 18:22:04 -0800

    Add alive status to current player

--- www/js/models/currentPlayer.js
+++ www/js/models/currentPlayer.js
@@ -2,6 +2,7 @@ define(['backbone'], function(Backbone){
   var currentPlayer = Backbone.Model.extend({
     initialize: function(){
       // Add game states
+      this.set('alive', true);
     }
   });
   return currentPlayer;
